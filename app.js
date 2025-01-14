58588aca9329614bb1d2ebe5e4fbd48ecb557f7f
Lucasmelander <95288911+Lucasmelander@users.noreply.github.com>
2025-01-14 12:12:21 +0100

    Add Boostrap

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -39,3 +41,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
