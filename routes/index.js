c5685684810d25df55ddf5d49b294a8f98878b04
Lucasmelander <95288911+Lucasmelander@users.noreply.github.com>
2025-01-16 13:05:35 +0100

    refactor recommendations

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data)});
+  // Read introduction data (assuming it's already being passed)
+  let introductionData = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  
+  // Read recommendations data
+  let recommendationsData = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+
+  // Pass both sets of data to the view
+  res.render('index', {
+    title: 'Portfolio',
+    array: JSON.parse(introductionData), // Existing data being passed
+    data: JSON.parse(recommendationsData) // New data being passed for recommendations
+  });
 });
 
 module.exports = router;
